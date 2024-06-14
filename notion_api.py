d9f59cf2ecdff1f3e3b3eb91f7413362ee17eea3
exanimoff <exanimoff@gmail.com>
2024-06-14 21:22:38 +0300

    filter database - stable

--- notion_api.py
+++ notion_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from config import NOTION_API_TOKEN, NOTION_API_BASE_URL
 
 headers = {
@@ -6,6 +7,12 @@ headers = {
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json"
 }
+
+
+def save_response_to_file(response, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(response, f, ensure_ascii=False, indent=4)
+
 
 def get_database(database_id):
     url = f"{NOTION_API_BASE_URL}/v1/databases/{database_id}"
@@ -16,12 +23,32 @@ def get_database(database_id):
     response.raise_for_status()
     return response.json()
 
+
 def create_database(parent_page_id, title, properties):
     url = f"{NOTION_API_BASE_URL}/v1/databases"
     data = {
         "parent": {"type": "page_id", "page_id": parent_page_id},
         "title": [{"type": "text", "text": {"content": title}}],
         "properties": properties
+    }
+    print(f"POST {url}")
+    print(f"Request data: {data}")
+    response = requests.post(url, headers=headers, json=data)
+    print(f"Response status: {response.status_code}")
+    print(f"Response content: {response.content}")
+    response.raise_for_status()
+    return response.json()
+
+
+def filter_database_task_completed(database_id):
+    url = f"{NOTION_API_BASE_URL}/v1/databases/{database_id}/query"
+    data = {
+        "filter": {
+            "property": "Status",
+            "status": {
+                "equals": "Done"
+            }
+        }
     }
     print(f"POST {url}")
     print(f"Request data: {data}")
--- tests/test_notion_api.py
+++ tests/test_notion_api.py
@@ -1,11 +1,26 @@
 import pytest
 import requests
-from notion_api import get_database, create_database
+from notion_api import *
 
 parent_page_id = "9a6a7be7edd349f588c9f066965d731c"
-PAGE_ID = "c860cb8ca7bf4e7eaba9d22fb9c61a57"
-DATABASE_ID = "c860cb8ca7bf4e7eaba9d22fb9c61a57"
+page_id = "c860cb8ca7bf4e7eaba9d22fb9c61a57"
+database_id = "c860cb8ca7bf4e7eaba9d22fb9c61a57"
 title = "Example Database"
+
+# def load_properties_from_file(filename):
+#     with open(filename, 'r', encoding='utf-8') as file:
+#         data = json.load(file)
+#     properties = data['properties']
+#     formatted_properties = {}
+#     for prop_name, prop_info in properties.items():
+#         if prop_info['type'] == 'status':
+#             formatted_properties[prop_name] = {'status': {}}
+#         else:
+#             formatted_properties[prop_name] = {prop_info['type']: prop_info[prop_info['type']]}
+#     return formatted_properties
+#
+# PROPERTIES = load_properties_from_file('database_response.json')
+
 PROPERTIES = {
         "Name": {
             "title": {}
@@ -21,12 +36,26 @@ PROPERTIES = {
     }
 
 def test_get_database():
-    response = get_database(DATABASE_ID)
+    """
+    Получает response и сохраняет в database_response.json
+    :return:
+    """
+    response = get_database(database_id)
+    save_response_to_file(response, 'database_response.json')
     assert response['object'] == 'database'
     assert 'properties' in response
-
-def test_create_page():
+#
+def test_create_database():
     response = create_database(parent_page_id, title, PROPERTIES)
     assert response['object'] == 'database'
+
     #assert response['properties']['Name']['title'][0]['text']['content'] == 'Test Page'
+
+def test_filter_database_task_completed():
+    response = filter_database_task_completed(database_id)
+    for result in response.get('results', []):
+        assert 'properties' in result
+        assert 'Status' in result['properties']
+        assert result['properties']['Status']['status']['name'] == 'Done'
+
 
